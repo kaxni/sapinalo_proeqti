af4d8be3d1b1ce6cb3d3a2f4c5cd13799e9fc5dd
kaxni <otikokaxniashvili8899@gmail.com>
2022-06-18 18:32:03 +0400

    add project

---
+++ js/main.js
@@ -0,0 +1,21 @@
+    /************** header on scroll  */
+    let header = document.querySelector('.header');
+    let headerHeight = header.clientHeight;
+ 
+    window.onscroll = function(){
+       if(window.pageYOffset > headerHeight){
+          header.classList.add('fixed');
+       }else {
+          header.classList.remove('fixed');
+       }
+    }
+    /************** burger menu  */
+ 
+    let headerBurger = document.querySelector('.header__burger')
+    let menu = document.querySelector('.menu');
+    let body = document.querySelector('body');
+    headerBurger.onclick = function() { 
+       headerBurger.classList.toggle('active');
+       menu.classList.toggle('active');
+       body.classList.toggle('no-scroll');
+    }
